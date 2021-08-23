4855050aa2a4e0d0f2940213c11b9509e55b565b
VlOrl <88439229+VlOrl@users.noreply.github.com>
2021-08-23 16:35:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.href_btn_input').click(function(event){
+    
+    $('.href_btn_input').addClass('active');
+
+});
+$('.list_lang').click(function(event){
+    
+    $('.list_lang,.sub_list_lng').toggleClass('active');
+
+});
